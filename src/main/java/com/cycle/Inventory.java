75909fc6500e53b2be7dfc68cf01f23edf7dd995
Puspak <puspak1947@gmail.com>
2025-09-22 21:40:04 +0530

    crate a inventory interface and implement all methods and successfully tests all methods

---
+++ src/main/java/com/cycle/Inventory.java
@@ -0,0 +1,16 @@
+
+package com.cycle;
+
+public interface Inventory {
+	void setCurrentStock(int currentStock);
+
+	int getCurrentStock();
+
+	void setAddStock(int addStock);
+
+	int getAddStock();
+
+	void addStock();
+
+	void purchase(int purchase);
+}
